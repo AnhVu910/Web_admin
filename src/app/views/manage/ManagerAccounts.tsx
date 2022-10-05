cf96bf80c05069e9866609dd915b42b2f91bf726
AnhVu910 <conanshinichi2000@gmail.com>
2022-10-05 18:31:43 +0700

    test

--- src/app/views/manage/ManagerAccounts.tsx
+++ src/app/views/manage/ManagerAccounts.tsx
@@ -3,5 +3,5 @@ import * as React from 'react'
 export interface ManagerAccountsProps {}
 
 export default function ManagerAccounts(props: ManagerAccountsProps) {
-  return <div>ManagerAccounts</div>
+  return <div>hell0</div>
 }
